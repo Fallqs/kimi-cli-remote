2b4b6497e6fa601126d5a9f3cdb6ef4ed1af7893
fallqs <fallqs@buaa.edu.cn>
2026-04-19 13:52:34 +0800

    fix(ssh): detect remote environment for system prompt when using SSH mode

--- src/kimi_cli/utils/environment.py
+++ src/kimi_cli/utils/environment.py
@@ -18,6 +18,12 @@ class Environment:
 
     @staticmethod
     async def detect() -> Environment:
+        from kaos import get_current_kaos
+        from kaos.ssh import SSHKaos
+
+        if isinstance(get_current_kaos(), SSHKaos):
+            return await Environment._detect_remote()
+
         match platform.system():
             case "Darwin":
                 os_kind = "macOS"
@@ -63,6 +69,52 @@ class Environment:
             else:
                 shell_name = "sh"
                 shell_path = fallback_path
+
+        return Environment(
+            os_kind=os_kind,
+            os_arch=os_arch,
+            os_version=os_version,
+            shell_name=shell_name,
+            shell_path=shell_path,
+        )
+
+    @staticmethod
+    async def _detect_remote() -> Environment:
+        """Detect environment on the remote host via SSH."""
+        import kaos
+
+        async def _run_remote(*args: str) -> str:
+            proc = await kaos.exec(*args)
+            data = await proc.stdout.read()
+            await proc.wait()
+            return data.decode("utf-8", errors="replace").strip()
+
+        os_kind_raw = await _run_remote("uname", "-s")
+        match os_kind_raw:
+            case "Darwin":
+                os_kind = "macOS"
+            case "Linux":
+                os_kind = "Linux"
+            case _:
+                os_kind = os_kind_raw
+
+        os_arch = await _run_remote("uname", "-m")
+        os_version = await _run_remote("uname", "-r")
+
+        possible_paths = [
+            KaosPath("/bin/bash"),
+            KaosPath("/usr/bin/bash"),
+            KaosPath("/usr/local/bin/bash"),
+        ]
+        fallback_path = KaosPath("/bin/sh")
+        for path in possible_paths:
+            if await path.is_file():
+                shell_name = "bash"
+                shell_path = path
+                break
+        else:
+            shell_name = "sh"
+            shell_path = fallback_path
 
         return Environment(
             os_kind=os_kind,
